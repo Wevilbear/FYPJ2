b02fa68a0431e802a73f6ccddf07b75442b65a4b
HaziqIrfan <wevilbear@mail.com>
2019-01-22 17:27:18 +0800

    Scripts
    InventoryInput.cs
    -Added toggle button I for inventory, C for equipment

---
+++ Company of Evil/Assets/Scripts/Inventory/InventoryInput.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryInput : MonoBehaviour
+{
+    [SerializeField] GameObject characterPanelGameObject;
+    [SerializeField] GameObject equipmentPanelGameObject;
+    [SerializeField] KeyCode[] toggleCharacterPanelKeys;
+    [SerializeField] KeyCode[] toggleInventoryKeys;
+
+    private void Update()
+    {
+        for (int i = 0; i < toggleCharacterPanelKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(toggleCharacterPanelKeys[i]))
+            {
+                characterPanelGameObject.SetActive(!characterPanelGameObject.activeSelf);
+
+                if (characterPanelGameObject.activeSelf)
+                {
+                    ShowMouseCursor();
+                }
+                else
+                {
+                    HideMouseCursor();
+                }
+                break;
+            }
+        }
+
+        for (int i = 0; i < toggleInventoryKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(toggleInventoryKeys[i]))
+            {
+                if (!characterPanelGameObject.activeSelf)
+                {
+                    characterPanelGameObject.SetActive(true);
+                    equipmentPanelGameObject.SetActive(false);
+                    ShowMouseCursor();
+
+                }
+                else if (equipmentPanelGameObject.activeSelf)
+                {
+                    equipmentPanelGameObject.SetActive(false);
+                }
+                else
+                {
+                    characterPanelGameObject.SetActive(false);
+                    HideMouseCursor();
+                }
+                break;
+            }
+        }
+    }
+
+    public void ShowMouseCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void HideMouseCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void ToggleEquipmentPanel()
+    {
+        equipmentPanelGameObject.SetActive(!equipmentPanelGameObject.activeSelf);
+    }
+}
